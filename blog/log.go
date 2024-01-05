be8620d7c5e5a22f0e3335e869816d8b3863a9f1
3052 <spen+3052@protonmail.com>
2024-01-04 18:50:53 -0600

    deleted:    blog/log.go

--- blog/log.go
+++ blog/log.go
@@ -4,22 +4,12 @@ import (
    "154.pages.dev/log"
    "flag"
    "log/slog"
-   "net/http"
 )
 
 func main() {
-   var h log.Handler
-   flag.TextVar(&h.Level, "v", h.Level, "log level")
+   var level log.Level
+   flag.TextVar(&level, "v", level, "level")
    flag.Parse()
-   log.Set_Handler(h)
-   log.Set_Transport(slog.LevelInfo)
-   slog.Info("hello world")
-   http.Get("http://example.com")
-   func() {
-      log.Set_Transport(slog.LevelDebug)
-      defer log.Set_Transport(slog.LevelInfo)
-      slog.Info("hello world")
-      http.Get("http://example.com")
-   }()
+   log.Set_Logger(level)
    slog.Debug("hello world")
 }
