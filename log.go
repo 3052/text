8de054d041bf08520dc9ce88db44c183685345c2
3052 <spen+3052@protonmail.com>
2024-02-08 22:17:08 -0600

    deleted:    progress.go

--- log.go
+++ log.go
@@ -1,40 +1,91 @@
 package log
 
 import (
+   "154.pages.dev/encoding"
    "context"
-   "fmt"
+   "io"
    "log/slog"
    "net/http"
+   "os"
+   "time"
 )
 
-func Set_Transport(level Level) {
-   http.DefaultClient.Transport = handler{level: level}
-}
-
-type Level = slog.Level
-
-type handler struct {
-   level Level
-   slog.Handler
-}
-
-func (h handler) Enabled(_ context.Context, level slog.Level) bool {
-   return level >= h.level
-}
-
-func (handler) Handle(_ context.Context, r slog.Record) error {
-   fmt.Print(r.Message)
-   r.Attrs(func(a slog.Attr) bool {
-      fmt.Print(" ", a.Key, ":", a.Value)
-      return true
+func Handler(v Level) {
+   th := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+      Level: slog.Level(v),
+      ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
+         switch a.Key {
+         case slog.LevelKey, slog.TimeKey:
+            return slog.Attr{}
+         }
+         return a
+      },
    })
-   fmt.Println()
-   return nil
+   slog.SetDefault(slog.New(th))
 }
 
-func (h handler) RoundTrip(req *http.Request) (*http.Response, error) {
+func TransportDebug() {
+   http.DefaultClient.Transport = Level(slog.LevelDebug)
+}
+
+func TransportInfo() {
+   http.DefaultClient.Transport = Level(slog.LevelInfo)
+}
+
+type Level slog.Level
+
+func (v Level) RoundTrip(r *http.Request) (*http.Response, error) {
    slog.Log(
-      context.Background(), h.level, "*", "method", req.Method, "URL", req.URL,
+      context.Background(), slog.Level(v), "request",
+      "method", r.Method, "URL", r.URL,
    )
-   return http.DefaultTransport.RoundTrip(req)
+   return http.DefaultTransport.RoundTrip(r)
+}
+
+type ProgressMeter struct {
+   first int
+   last int64
+   length int64
+   parts struct {
+      last int64
+      length int64
+   }
+   modified time.Time
+   date time.Time
+}
+
+func (p *ProgressMeter) Reader(res *http.Response) io.Reader {
+   p.parts.last += 1
+   p.last += res.ContentLength
+   p.length = p.last * p.parts.length / p.parts.last
+   return io.TeeReader(res.Body, p)
+}
+
+func (p *ProgressMeter) Set(parts int) {
+   p.date = time.Now()
+   p.modified = time.Now()
+   p.parts.length = int64(parts)
+}
+
+func (p *ProgressMeter) Write(data []byte) (int, error) {
+   p.first += len(data)
+   if time.Since(p.modified) >= time.Second {
+      slog.Info(
+         "progress", "%", p.percent(), "size", p.size(), "rate", p.rate(),
+      )
+      p.modified = time.Now()
+   }
+   return len(data), nil
+}
+
+func (p ProgressMeter) percent() encoding.Percent {
+   return encoding.Percent(p.first) / encoding.Percent(p.length)
+}
+
+func (p ProgressMeter) rate() encoding.Rate {
+   return encoding.Rate(p.first) / encoding.Rate(time.Since(p.date).Seconds())
+}
+
+func (p ProgressMeter) size() encoding.Size {
+   return encoding.Size(p.first)
 }
--- log_test.go
+++ log_test.go
@@ -3,19 +3,20 @@ package log
 import (
    "io"
    "net/http"
-   "log/slog"
    "testing"
 )
 
 const address = "https://go.dev/dl/go1.21.5.windows-amd64.zip"
 
-func Test_Progress(t *testing.T) {
-   Set_Transport(slog.LevelInfo)
+func TestSlog(t *testing.T) {
+   Handler(0)
+   TransportInfo()
+   var meter ProgressMeter
+   meter.Set(1)
    res, err := http.Get(address)
    if err != nil {
       t.Fatal(err)
    }
    defer res.Body.Close()
-   r := New_Progress(1).Reader(res)
-   io.Copy(io.Discard, r)
+   io.Copy(io.Discard, meter.Reader(res))
 }
