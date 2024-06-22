a637993f5550ecc175d9eaff18c3b8a25548ce60
3052 <spen+3052@protonmail.com>
2024-06-22 08:44:43 -0500

    +   resp, err := http.Get(address)

--- log_test.go
+++ log_test.go
@@ -11,24 +11,24 @@ const address = "https://go.dev/dl/go1.21.5.windows-amd64.zip"
 func TestOne(t *testing.T) {
    var meter ProgressMeter
    meter.Set(1)
-   res, err := http.Get(address)
+   resp, err := http.Get(address)
    if err != nil {
       t.Fatal(err)
    }
-   defer res.Body.Close()
-   io.Copy(io.Discard, meter.Reader(res))
+   defer resp.Body.Close()
+   io.Copy(io.Discard, meter.Reader(resp))
 }
 
 func TestTwo(t *testing.T) {
    var meter ProgressMeter
    meter.Set(1)
    LogLevel{}.SetTransport(true)
-   res, err := http.Get(address)
+   resp, err := http.Get(address)
    if err != nil {
       t.Fatal(err)
    }
-   defer res.Body.Close()
-   io.Copy(io.Discard, meter.Reader(res))
+   defer resp.Body.Close()
+   io.Copy(io.Discard, meter.Reader(resp))
 }
 
 func TestThree(t *testing.T) {
@@ -37,10 +37,10 @@ func TestThree(t *testing.T) {
    var log LogLevel
    log.Set()
    log.SetTransport(true)
-   res, err := http.Get(address)
+   resp, err := http.Get(address)
    if err != nil {
       t.Fatal(err)
    }
-   defer res.Body.Close()
-   io.Copy(io.Discard, meter.Reader(res))
+   defer resp.Body.Close()
+   io.Copy(io.Discard, meter.Reader(resp))
 }
