e01314461efaeaeb68c52c0ce3d3b0d308e61624
3052 <spen+3052@protonmail.com>
2024-06-26 19:32:07 -0500

    +func TestRedirect() {

--- log_test.go
+++ log_test.go
@@ -6,9 +6,33 @@ import (
    "testing"
 )
 
+func TestClient(t *testing.T) {
+   new(Transport).Set()
+   resp, err := http.Get("http://go.dev")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer resp.Body.Close()
+   io.Copy(io.Discard, resp.Body)
+}
+
+func TestTransport(t *testing.T) {
+   new(Transport).Set()
+   req, err := http.NewRequest("", "http://go.dev", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   resp, err := http.DefaultTransport.RoundTrip(req)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer resp.Body.Close()
+   io.Copy(io.Discard, resp.Body)
+}
+
 const address = "https://go.dev/dl/go1.21.5.windows-amd64.zip"
 
-func TestOne(t *testing.T) {
+func TestMeterOne(t *testing.T) {
    var meter ProgressMeter
    meter.Set(1)
    resp, err := http.Get(address)
@@ -19,24 +43,10 @@ func TestOne(t *testing.T) {
    io.Copy(io.Discard, meter.Reader(resp))
 }
 
-func TestTwo(t *testing.T) {
+func TestMeterTwo(t *testing.T) {
    var meter ProgressMeter
    meter.Set(1)
-   LogLevel{}.SetTransport(true)
-   resp, err := http.Get(address)
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer resp.Body.Close()
-   io.Copy(io.Discard, meter.Reader(resp))
-}
-
-func TestThree(t *testing.T) {
-   var meter ProgressMeter
-   meter.Set(1)
-   var log LogLevel
-   log.Set()
-   log.SetTransport(true)
+   new(Transport).Set()
    resp, err := http.Get(address)
    if err != nil {
       t.Fatal(err)
