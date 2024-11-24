53fc18c991835b46dbfb612fc15943ebe6831779
3052 <spen+3052@protonmail.com>
2024-11-24 08:45:28 -0600

    deleted:    name.go

--- text.go
+++ text.go
@@ -7,9 +7,60 @@ import (
    "log/slog"
    "net/http"
    "strconv"
+   "strings"
+   "text/template"
    "time"
 )
 
+var DefaultName =
+   "{{if .Show}}" +
+      "{{if .Season}}" +
+         "{{if .Title}}" +
+            "{{.Show}} - {{.Season}} {{.Episode}} - {{.Title}}" +
+         "{{else}}" +
+            "{{.Show}} - {{.Season}} {{.Episode}}" +
+         "{{end}}" +
+      "{{else}}" +
+         "{{.Show}} - {{.Title}}" +
+      "{{end}}" +
+   "{{else}}" +
+      "{{if .Year}}" +
+         "{{.Title}} - {{.Year}}" +
+      "{{else}}" +
+         "{{.Title}}" +
+      "{{end}}" +
+   "{{end}}"
+
+func Clean(s string) string {
+   mapping := func(r rune) rune {
+      if strings.ContainsRune(`"*/:<>?\|`, r) {
+         return '-'
+      }
+      return r
+   }
+   return strings.Map(mapping, s)
+}
+
+func Name(n Namer) (string, error) {
+   text, err := new(template.Template).Parse(DefaultName)
+   if err != nil {
+      return "", err
+   }
+   var b strings.Builder
+   err = text.Execute(&b, n)
+   if err != nil {
+      return "", err
+   }
+   return b.String(), nil
+}
+
+type Namer interface {
+   Show() string
+   Season() int
+   Episode() int
+   Title() string
+   Year() int
+}
 func (p *ProgressMeter) Set(parts int) {
    p.date = time.Now()
    p.modified = time.Now()
