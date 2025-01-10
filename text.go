6cca8ee14b26a997c394921a0560424eb2dbd946
3052 <spen+3052@protonmail.com>
2025-01-09 20:04:36 -0600

    deleted:    name.go

--- text.go
+++ text.go
@@ -2,6 +2,7 @@ package text
 
 import (
    "bytes"
+   "fmt"
    "io"
    "log"
    "log/slog"
@@ -11,6 +12,38 @@ import (
    "time"
 )
 
+type Namer interface {
+   Show() string
+   Season() int
+   Episode() int
+   Title() string
+   Year() int
+}
+
+func Name(n Namer) string {
+   var data []byte
+   if n.Show() != "" {
+      data = fmt.Append(data, n.Show(), " - ")
+      if n.Season() >= 1 {
+         data = fmt.Append(data, n.Season(), " ", n.Episode())
+         if n.Title() != "" {
+            data = fmt.Append(data, " - ", n.Title())
+         }
+      } else {
+         if n.Episode() >= 1 {
+            data = fmt.Append(data, n.Episode(), " - ", n.Title())
+         } else {
+            data = append(data, n.Title()...)
+         }
+      }
+   } else {
+      data = append(data, n.Title()...)
+      if n.Year() >= 1 {
+         data = fmt.Append(data, " - ", n.Year())
+      }
+   }
+   return string(data)
+}
 func Clean(s string) string {
    mapping := func(r rune) rune {
       if strings.ContainsRune(`"*/:<>?\|`, r) {
--- text_test.go
+++ text_test.go
@@ -7,6 +7,66 @@ import (
    "testing"
 )
 
+var name_tests = []test_alfa{
+   {
+      title: "title",
+      year: 2024,
+   },
+   {
+      show: "show",
+      title: "title",
+   },
+   {
+      show: "show",
+      episode: 3,
+      title: "title",
+   },
+   {
+      show: "show",
+      season: 2,
+      episode: 3,
+      title: "title",
+   },
+}
+
+type test_alfa struct {
+   show string
+   season int
+   episode int
+   title string
+   year int
+}
+
+type test_bravo struct {
+   t test_alfa
+}
+
+func (t test_bravo) Show() string {
+   return t.t.show
+}
+
+func (t test_bravo) Season() int {
+   return t.t.season
+}
+
+func (t test_bravo) Episode() int {
+   return t.t.episode
+}
+
+func (t test_bravo) Title() string {
+   return t.t.title
+}
+
+func (t test_bravo) Year() int {
+   return t.t.year
+}
+
+func TestName(t *testing.T) {
+   for _, test := range name_tests {
+      bravo := test_bravo{test}
+      fmt.Printf("%q\n", Name(bravo))
+   }
+}
 const address = "https://dl.google.com/go/go1.21.5.windows-amd64.zip"
 
 func TestMeter(t *testing.T) {
