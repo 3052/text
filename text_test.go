814b57dda33611568b10b6358849f73e386b399f
3052 <spen+3052@protonmail.com>
2025-01-11 00:07:56 -0600

    +func TestTransport(t *testing.T) {

--- text_test.go
+++ text_test.go
@@ -2,8 +2,68 @@ package text
 
 import (
    "fmt"
+   "log"
+   "net/http"
+   "strings"
    "testing"
 )
+
+func TestTransport(t *testing.T) {
+   var out strings.Builder
+   log.SetOutput(&out)
+   Transport{}.Set()
+   _, err := http.Head("http://example.com")
+   if err != nil {
+      t.Fatal(err)
+   }
+   if !strings.HasSuffix(out.String(), " INFO HEAD URL=http://example.com\n") {
+      t.Fatal(&out)
+   }
+}
+
+func TestSize(t *testing.T) {
+   for _, test := range size_tests {
+      if fmt.Sprint(test.in) != test.out {
+         t.Fatal(test)
+      }
+   }
+}
+
+func TestRate(t *testing.T) {
+   for _, test := range rate_tests {
+      if fmt.Sprint(test.in) != test.out {
+         t.Fatal(test)
+      }
+   }
+}
+
+var rate_tests = []struct{
+   in Rate
+   out string
+}{
+   {123.45, "123 byte/s"},
+   {123.45*1000, "123.45 kilobyte/s"},
+   {123.45*1000*1000, "123.45 megabyte/s"},
+   {123.45*1000*1000*1000, "123.45 gigabyte/s"},
+}
+
+var size_tests = []struct{
+   in Size
+   out string
+}{
+   {123.45, "123 byte"},
+   {123.45*1000, "123.45 kilobyte"},
+   {123.45*1000*1000, "123.45 megabyte"},
+   {123.45*1000*1000*1000, "123.45 gigabyte"},
+}
+
+func TestCardinal(t *testing.T) {
+   for _, test := range cardinal_tests {
+      if fmt.Sprint(test.in) != test.out {
+         t.Fatal(test)
+      }
+   }
+}
 
 var cardinal_tests = []struct{
    in Cardinal
@@ -15,10 +75,17 @@ var cardinal_tests = []struct{
    {123.45*1000*1000*1000, "123.45 billion"},
 }
 
-func TestCardinal(t *testing.T) {
-   for _, test := range cardinal_tests {
-      out := fmt.Sprint(test.in)
-      if out != test.out {
+var percent_tests = []struct{
+   in Percent
+   out string
+}{
+   {0.0123, "1.23 %"},
+   {0.1234, "12.34 %"},
+}
+
+func TestPercent(t *testing.T) {
+   for _, test := range percent_tests {
+      if fmt.Sprint(test.in) != test.out {
          t.Fatal(test)
       }
    }
